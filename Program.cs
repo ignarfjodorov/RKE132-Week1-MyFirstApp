4d04ced81a6130a058977f4e3da3b2adf99fa0d3
ignarfjodorov <ignar.fjodorov@gmail.com>
2022-09-04 20:03:14 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+// Rakendus küsib kasutajal sisestada tema nimi
+// Rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Väljund
+
+// String- SÕNE
+string userName = Console.ReadLine(); //Sisend
+
+Console.WriteLine("Hello" + ", " + userName + "!"); //Väljund
+
+// String - interpolation
+Console.WriteLine($"Hello, {userName}!"); //Väljund
+
